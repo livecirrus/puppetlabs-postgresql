d5e8a9e0598e3d5a76654e68a1dae9d8ceb2fb67
Lida Tang <lida.tang@gmail.com>
2014-04-21 00:30:06 -0400

    if ensure is manual, we restart using pg_ctlcluster

--- manifests/server/reload.pp
+++ manifests/server/reload.pp
@@ -1,15 +1,26 @@
 # PRIVATE CLASS: do not use directly
 class postgresql::server::reload {
   $ensure         = $postgresql::server::ensure
+  $enable         = $postgresql::server::enable
   $service_name   = $postgresql::server::service_name
   $service_status = $postgresql::server::service_status
-
+  $version        = $postgresql::server::version
   if($ensure == 'present' or $ensure == true) {
-    exec { 'postgresql_reload':
-      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-      command     => "service ${service_name} reload",
-      onlyif      => $service_status,
-      refreshonly => true,
+    if ($enable == 'manual') {
+      exec { 'postgresql_reload':
+        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command     => "pg_ctlcluster ${version} main reload",
+        onlyif      => $service_status,
+        refreshonly => true,
+      }
+    }      
+    else {
+	    exec { 'postgresql_reload':
+	      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+	      command     => "service ${service_name} reload",
+	      onlyif      => $service_status,
+	      refreshonly => true,
+	    }
     }
   }
 }
